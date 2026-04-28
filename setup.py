7ce5917de36e17aadfa379078336e654d6b7578c
shiyanthan-intelliinspire <shiyanthan@intelliinspire.com>
2026-04-28 13:49:48 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Clustersco",
+    author_email="shiyanthan@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
